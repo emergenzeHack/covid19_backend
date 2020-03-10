682096461f4c3daad55ee0cb6fdd129aa9adc898
Antonio Vivace <avivace4@gmail.com>
2020-03-10 18:26:03 +0100

    Titolo e label

--- server.py
+++ server.py
@@ -21,13 +21,33 @@ def hello_world():
 @app.route('/report', methods=['POST'])
 def process_report():
     payload = request.json
-    yaml_payload = "```\n"+yaml.dump(payload)+"\n```"
+    stripped_payload = strip_meta(payload)
+    yaml_payload = "```\n"+yaml.dump(stripped_payload)+"```"
     print(yaml_payload)
-    issue_type=request.headers.get('type')
-    open_github_issue(title=issue_type, body=yaml_payload)
+    label=request.headers.get('label')
+    if "cosa" in list(payload):
+        issue_title=payload["cosa"][0:50]
+    elif "testo" in list(payload):
+        issue_title=payload["testo"][0:50]
+    elif "descrizione" in list(payload):
+        issue_title=payload["descrizione"][0:50]
+    else:
+        issue_title=label
+    open_github_issue(title=issue_title, body=yaml_payload, labels=[label, "form"])
     return "OK", 200
 
-def open_github_issue(title, body=None, assignee=None, milestone=None, labels=["provaform"]):
+def strip_meta(payload):
+    excludelist = ["end", "start", "formhub/uuid", "meta/instanceID"]
+    # Rimuovi tutti i campi meta che iniziano con _
+    for k in list(payload):
+        if k.startswith('_'):
+            payload.pop(k)
+        # Rimuovi anche le chiavi specificate nella lista
+        elif k in excludelist:
+            payload.pop(k)
+    return payload
+
+def open_github_issue(title, body=None, assignee=None, milestone=None, labels=[]):
     '''Create an issue on github.com using the given parameters.'''
     # Our url to create issues via POST
     url = 'https://api.github.com/repos/%s/%s/issues' % (REPO_OWNER, REPO_NAME)
--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
-curl --header "type: test"\
+curl --header "label: provaform"\
   --header "Content-Type: application/json" \
   --request POST \
-  --data '{"testo":"xyz","link":"xyz"}' \
+  --data '{"cosa":"xyz","link":"xyz"}' \
   http://localhost:5000/report
 
 # 167.172.98.164
