f3323962cfcd5a9b0ef92ebd19c9c3c4f86ba81f
Antonio Vivace <avivace4@gmail.com>
2020-03-10 17:18:43 +0100

    Test POST for development

---
+++ test.sh
@@ -0,0 +1,7 @@
+curl --header "type: test"\
+  --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"testo":"xyz","link":"xyz"}' \
+  http://localhost:5000/report
+
+# 167.172.98.164
